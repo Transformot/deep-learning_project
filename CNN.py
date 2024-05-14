af313b4415a8e8e7df7487aeeb2f197d879a51b4
sicard <arthur.srd18@gmail.com>
2024-05-14 15:49:50 +0200

    ok

--- CNN.py
+++ CNN.py
@@ -1,18 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
 import tensorflow as tf
-
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input, Conv1D, Activation, MaxPooling1D, Flatten, Dense, Dropout
-from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.models import Model
-
 import time
-
-from sklearn.metrics import confusion_matrix
-import seaborn as sns
-
 import random
 
 train_data = np.loadtxt('data/ECG200_TRAIN.tsv', delimiter='\t')
@@ -36,41 +28,39 @@ y_train_encoded = to_categorical(y_train, num_classes=2)
 y_test_encoded = to_categorical(y_test, num_classes=2)
 
 
-filters = 5
-kernel_size = 3
+filters = 1
+kernel_size = 1
 stride = 1
 padding = 'same'
 use_bias = False
 hidden_activation = 'relu'
-final_activation = 'softmax'
-pool_size = 3
-dropout_rate = 0.3
+final_activation = 'sigmoid'
+pool_size = 1
+dropout_rate = 0.2
 nb_classes = 2 
-learning_rate = 0.0005
-optimizer_algo = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+# learning_rate = 0.001
+optimizer_algo = tf.keras.optimizers.Adam()  # learning_rate=learning_rate
 cost_function = tf.keras.losses.categorical_crossentropy
 mini_batch_size = 16
-nb_epochs = 500
+nb_epochs = 400
 percentage_of_train_as_validation = 0.2
 
 # Définir les plages de valeurs pour les hyperparamètres
 filters_range = [5]
-kernel_size_range = [2]                           # [1, 2, 3]
-strides_range = [2]                           # [1, 2, 3]
-final_activation_range = ['softmax']             # ['softmax', 'sigmoid']
-pool_size_range = [2]                                 # [1, 2, 3]
-dropout_rate_range = [0.25]
-learning_rate_range = [0.006]
-mini_batch_size_range = [32]
+kernel_size_range = [3]                        # [1, 2, 3]
+strides_range = [1]                          # [1, 2, 3]
+pool_size_range = [2]                                # [1, 2, 3]
+dropout_rate_range = [0.2]
+mini_batch_size_range = [16, 32]
 
 # Nombre total de combinaisons à essayer
 total_combinations = len(filters_range) * len(kernel_size_range) * len(strides_range) \
-                   * len(final_activation_range) * len(pool_size_range) \
-                   * len(dropout_rate_range) * len(learning_rate_range) * len(mini_batch_size_range)
+                   * len(pool_size_range) \
+                   * len(dropout_rate_range) * len(mini_batch_size_range)
 print(total_combinations)
 
 # Nombre de combinaisons à essayer
-num_trials = 10
+num_trials = 60*1
 
 start_time = time.time()
 best_accuracy = 0
@@ -83,10 +73,8 @@ for i in range(num_trials):
     filters = random.choice(filters_range)
     kernel_size = random.choice(kernel_size_range)
     stride = random.choice(strides_range)
-    final_activation = random.choice(final_activation_range)
     pool_size = random.choice(pool_size_range)
     dropout_rate = random.choice(dropout_rate_range)
-    learning_rate = random.choice(learning_rate_range)
     mini_batch_size = random.choice(mini_batch_size_range)
     
     # Construire, compiler et entraîner le modèle avec ces hyperparamètres
@@ -97,25 +85,21 @@ for i in range(num_trials):
     relu_layer_1_1 = Activation(hidden_activation)(conv_layer_1_1)
     conv_layer_1_2 = Conv1D(filters=filters,kernel_size=kernel_size,strides=stride,padding=padding,use_bias=use_bias)(relu_layer_1_1)
     relu_layer_1_2 = Activation(hidden_activation)(conv_layer_1_2)
-    conv_layer_1_3 = Conv1D(filters=filters,kernel_size=kernel_size,strides=stride,padding=padding,use_bias=use_bias)(relu_layer_1_2)
-    relu_layer_1_3 = Activation(hidden_activation)(conv_layer_1_3)
-    pooling_layer_1 = MaxPooling1D(pool_size=pool_size, padding=padding)(relu_layer_1_3)
+    pooling_layer_1 = MaxPooling1D(pool_size=pool_size, padding=padding)(relu_layer_1_2)
     dropout_layer_1_1 = Dropout(rate=dropout_rate)(pooling_layer_1)
 
     conv_layer_2_1 = Conv1D(filters=filters,kernel_size=kernel_size,strides=stride,padding=padding,use_bias=use_bias)(dropout_layer_1_1)
     relu_layer_2_1 = Activation(hidden_activation)(conv_layer_2_1)
     conv_layer_2_2 = Conv1D(filters=filters,kernel_size=kernel_size,strides=stride,padding=padding,use_bias=use_bias)(relu_layer_2_1)
     relu_layer_2_2 = Activation(hidden_activation)(conv_layer_2_2)
-    conv_layer_2_3 = Conv1D(filters=filters,kernel_size=kernel_size,strides=stride,padding=padding,use_bias=use_bias)(relu_layer_2_2)
-    relu_layer_2_3 = Activation(hidden_activation)(conv_layer_2_3)
-    pooling_layer_2 = MaxPooling1D(pool_size=pool_size, padding=padding)(relu_layer_2_3)
+    pooling_layer_2 = MaxPooling1D(pool_size=pool_size, padding=padding)(relu_layer_2_2)
 
     flattened_layer = Flatten()(pooling_layer_2)
     dropout_flattened = Dropout(rate=dropout_rate)(flattened_layer)
     output_layer = Dense(units=nb_classes,activation=final_activation)(dropout_flattened)
 
     model = Model(inputs=input_layer, outputs=output_layer)
-    optimizer_algo = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    optimizer_algo = tf.keras.optimizers.Adam()
     model.compile(loss=cost_function,optimizer=optimizer_algo, metrics=['accuracy'])
     model_checkpoint = tf.keras.callbacks.ModelCheckpoint('best_model_CNN.keras', monitor='val_loss', save_best_only=True)
     history = model.fit(X_train_normalized, y_train_encoded, 
@@ -127,15 +111,17 @@ for i in range(num_trials):
     
     # Évaluer la performance du meilleur modèle
     best_model = tf.keras.models.load_model('best_model_CNN.keras')
+    train_loss, train_accuracy = best_model.evaluate(X_train_normalized, y_train_encoded)
     test_loss, test_accuracy = best_model.evaluate(X_test_normalized, y_test_encoded)
+    print((filters, kernel_size, stride, pool_size, dropout_rate, mini_batch_size))
     
     # Mettre à jour les meilleurs hyperparamètres si nécessaire
     if test_accuracy - test_loss > best_accuracy - best_loss :
         best_accuracy = test_accuracy
         best_loss = test_loss
-        best_hyperparameters.append((filters, kernel_size, stride, final_activation, pool_size, dropout_rate, learning_rate, mini_batch_size))
+        best_hyperparameters.append((filters, kernel_size, stride, pool_size, dropout_rate, mini_batch_size))
     if test_accuracy - test_loss > 0.47 :
-        best_hyperparameters_2.append((filters, kernel_size, stride, final_activation, pool_size, dropout_rate, learning_rate, mini_batch_size))
+        best_hyperparameters_2.append((filters, kernel_size, stride, pool_size, dropout_rate, mini_batch_size))
 
 # Fin du compteur de temps
 end_time = time.time()
